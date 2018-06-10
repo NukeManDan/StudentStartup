eca5273511ffee9d3430115f14af59a178150ada
Dan <35669742+NukeManDan@users.noreply.github.com>
2018-06-09 23:52:57 -0600

    Update Slogan.jsx

--- src/components/Slogan.jsx
+++ src/components/Slogan.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Slogan = () => {
   return (
     <div className="slogan">
-      <h2>Pioneering Solutions To Problem-Based Learning.</h2>
+      <h2>Pioneering Solutions To Problem-Based Learning</h2>
       <hr />
     </div>
   );
